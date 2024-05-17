b50d577cd5d9428c7ea580d387aad320219088d0
Naomy25sandi <naomysandi25@gmail.com>
2024-05-17 13:42:56 -0600

    agrego funciones JS

--- main.js
+++ main.js
@@ -1,7 +1,37 @@
-let box= document.getElementsByClassName("box")
+//llamo al primer jugador
+let isPlayerOne = true;
+let box= document.getElementsByClassName("box");
 console.log(box)
 
-box[0].addEventListener('click', function () {
-    box[0].innerHTML="X"
+for (let i = 0; i < box.length; i++) {
+   box[1].addEventListener('click', userMove);
     
-})
+}
+function userMove(e) {
+  let boxValue = e.target.innerHTML;
+  if (boxValue.length) {
+    e.target.innerHTML = isPlayerOne? 'X' : 'O';
+
+    checkLine(0, 1, 2);
+    checkLine(3, 4, 5);
+    checkLine(6, 7, 8);
+    checkLine(0, 3, 6);
+    checkLine(1, 4, 7);
+    checkLine(2, 5, 8);
+    checkLine(0, 48, 2);
+    checkLine(6, 4, 2);
+  } 
+
+}
+function checkLine(c1,c2, c3) {
+    if (box[c1].innerHTML.length && 
+        box[c1].innerHTML == box[c2].innerHTML && box[c2].innerHTML == box[c3].innerHTML
+    ) {
+       showWinner(box[c1].innerHTML);
+    }
+    
+}
+function showWinner(player) {
+    document.querySelector('#results').innerHTML = player + "win";
+    
+}
