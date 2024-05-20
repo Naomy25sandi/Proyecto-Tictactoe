366b408b42e886708656581a8d3342601023abcb
Naomy25sandi <naomysandi25@gmail.com>
2024-05-20 15:49:26 -0600

    cambios

--- main.js
+++ main.js
@@ -7,17 +7,29 @@ let casilla5 = document.getElementById('b5');
 let casilla6 = document.getElementById('b6');
 let casilla7 = document.getElementById('b7');
 let casilla8 = document.getElementById('b8');
-
+// hago una variable donde incluyan el array de las casillas//
 let tablero = [casilla0,casilla1,casilla2,casilla3,casilla4,casilla5,casilla6,casilla7,casilla8]
 
-
+//  llamo la funcion y la recorro con el forEach y agrego el evento con el click//
 function juego() {
   tablero.forEach(celda=>celda.addEventListener("click",function(){
     celda.innerHTML = "x"
+    maquina()
   }))
+}
+juego()
+
+function turno(max) {
+  return Math.floor(Math.random()*max);
 }
 
 function juegoPc() {
-  
+   
+  let jugador = turno(9);
+  console.log(jugador);
+
+  if (!celda) {
+    
+  }
 }
-juego()
+
