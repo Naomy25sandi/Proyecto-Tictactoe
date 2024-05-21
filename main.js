aa12990d9692be8540b22429d54ec883e531a374
Naomy25sandi <naomysandi25@gmail.com>
2024-05-21 11:56:29 -0600

    cambio js

--- main.js
+++ main.js
@@ -9,27 +9,21 @@ let casilla7 = document.getElementById('b7');
 let casilla8 = document.getElementById('b8');
 // hago una variable donde incluyan el array de las casillas//
 let tablero = [casilla0,casilla1,casilla2,casilla3,casilla4,casilla5,casilla6,casilla7,casilla8]
-
+let ganador = document.getElementById('result');// funcion para ver el ganador.
 //  llamo la funcion y la recorro con el forEach y agrego el evento con el click//
 function juego() {
-  tablero.forEach(celda=>celda.addEventListener("click",function(){
-    celda.innerHTML = "x"
+   tablero.forEach(celda=>celda.addEventListener("click", function() {
+    celda.innerHTML = "❌"
     maquina()
   }))
 }
 juego()
 
-function turno(max) {
-  return Math.floor(Math.random()*max);
-}
-
-function juegoPc() {
+function maquina() {
+  let vacio = tablero.filter(cell=>cell.innerHTML == "");
+  let numAleatorio = Math.floor(Math.random()*vacio.length)
+  if (vacio.length > 0) {
+    vacio[numAleatorio].innerHTML = "⭕";
    
-  let jugador = turno(9);
-  console.log(jugador);
-
-  if (!celda) {
-    
   }
 }
-
