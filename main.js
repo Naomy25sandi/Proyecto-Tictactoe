661743e64bb09e6d4da551abefd5dd94a0ea7b2e
Naomy25sandi <naomysandi25@gmail.com>
2024-05-22 15:03:51 -0600

    js

--- main.js
+++ main.js
@@ -7,26 +7,65 @@ let casilla5 = document.getElementById('casilla5');
 let casilla6 = document.getElementById('casilla6');
 let casilla7 = document.getElementById('casilla7');
 let casilla8 = document.getElementById('casilla8');
+let iniciar = document.getElementById('start');
 
+iniciar.addEventListener('click', _=>{
+  location.reload();
+}
+)
 // hago una variable donde incluyan el array de las casillas//
 let tablero = [casilla0, casilla1, casilla2, casilla3, casilla4, casilla5, casilla6, casilla7, casilla8]
-
 
 //  llamo la funcion y la recorro con el forEach y agrego el evento con el click//
 function juego() {
   tablero.forEach(celda => celda.addEventListener("click", function () {
     celda.innerHTML = "❌"
     maquina()
-  }))
 
+    // si hay ganador
+    if (checkLine()){}
+  }))
 }
 juego()
-// llamo la funcion para que la maquina use su turno.
+
 function maquina() {
-  let vacio = tablero.filter(cel => cel.innerHTML == "");
-  let numAleatorio = Math.floor(Math.random() * vacio.length)
-  if (vacio.length > 0) {
-    vacio[numAleatorio].innerHTML = "⭕"
-  }
+  setTimeout(() => {
+    let vacio = tablero.filter(cel => cel.innerHTML == "");
+    let numAleatorio = Math.floor(Math.random() * vacio.length)
+    if (vacio.length > 0) {
+      vacio[numAleatorio].innerHTML = "⭕"
+    }
+  }, 1000);
 }
 
+
+let contenidoGnador;
+
+
+ //funcion para ganador
+function checkLine() {
+  const posGanadoras = [
+    [0,1,2],    //FILAS
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],    //COLUMNAS 
+    [1,4,7],
+    [2,5,8],    
+    [0,4,8],   //DIAGONALES
+    [2,4,6]
+    ] 
+    for (const pos of posGanadoras) {// se crea la funcion de las posiciones ganadoras
+      [a,b,c] = pos
+      if(tablero[a].innerHTML && tablero[a].innerHTML === tablero[b].innerHTML && tablero[a].innerHTML==tablero[c].innerHTML){
+          
+        if (tablero[a].innerHTML == '❌') {
+          alert("VICTORIA "+'❌')  
+        }else{
+          alert("VICTORIA "+'')  
+        }
+        return true
+        
+        
+      }   
+}
+}
